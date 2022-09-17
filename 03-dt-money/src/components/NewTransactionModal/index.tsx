0e95117b85eb563676a9749b542bd5ffb71a29fe
Michael Douglas <michaeldougls93@gmail.com>
2022-09-17 21:40:43 +0100

    modal de nova transacao finalizado

--- 03-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,18 +1,37 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Overlay,Content } from "./styles";
-
+import { X } from "phosphor-react";
+import { Overlay,Content, CloseButton, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleUp, ArrowCircleDown } from 'phosphor-react'
 export function NewTransactionModal() {
   return (
     <Dialog.Portal>
       <Overlay />
+
       <Content>
         <Dialog.Title>Nova Transacao</Dialog.Title>
-        <Dialog.Close />
+        <CloseButton>
+          <X size={24}/>
+        </CloseButton>
+        
         <form action="">
             <input type="text" placeholder="Descricao" required/>
             <input type="number" placeholder="Preco" required/>
             <input type="text" placeholder="Categoria" required/>
-            <button type="reset">Cadastrar</button>
+
+
+            <TransactionType>
+              <TransactionTypeButton variant="income" value="income">
+                Entrada
+                <ArrowCircleUp size={24} />
+              </TransactionTypeButton>
+              <TransactionTypeButton variant="outcome" value="outcome">
+                Saida
+                <ArrowCircleDown size={24}/>
+              </TransactionTypeButton>
+            </TransactionType>
+            <button className="button" type="submit">
+              Cadastrar
+            </button>
         </form>
 
       </Content>
